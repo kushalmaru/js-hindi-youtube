353af0a5dab8dbba1b0582e2408362a965f1e87a
kushalmaru <122462086+kushalmaru@users.noreply.github.com>
2024-06-01 01:07:44 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33abc";
+console.log(typeof score);
+
+
+let valueInNmber = Number(score);
+console.log(typeof valueInNmber);
+console.log(valueInNmber);
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1
+//  false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// "" => false
+// "Kushal" => true
