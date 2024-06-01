672aa2cfdc99a164b0d7c6a23746983416cb0517
kushalmaru <122462086+kushalmaru@users.noreply.github.com>
2024-06-01 09:58:11 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,18 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(4));
+
+// +++++++++++++++++++++++ Math ++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+
+console.log(Math.random());
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) +  min);
