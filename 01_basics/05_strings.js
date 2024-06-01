765d2c640251db2c08b9b30afcb9f0242360188c
kushalmaru <122462086+kushalmaru@users.noreply.github.com>
2024-06-01 09:34:36 +0000

    types of strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+const name = "kushal";
+const repoCount = 50;
+
+// console.log(name + repoCount + " Value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('kushalmaru') // another way to declare strings. Imp as an object
+console.log(gameName[0]);
+console.log(gameName.__proto__);
