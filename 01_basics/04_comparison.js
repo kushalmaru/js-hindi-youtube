afe80f98ddc8fb771a417da9c3bc3681897e7cbf
kushalmaru <122462086+kushalmaru@users.noreply.github.com>
2024-06-01 02:23:12 +0000

    Conversion of datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+const id = Symbol('123');
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
